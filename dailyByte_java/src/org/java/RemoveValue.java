40f0df6766fe3657eac80301e2025f11492dfeed
giuseppevignanello <peppe.vignanello@gmail.com>
2023-11-05 19:46:52 +0100

    try remove value without List

--- dailyByte_java/src/org/java/RemoveValue.java
+++ dailyByte_java/src/org/java/RemoveValue.java
@@ -13,20 +13,34 @@ public class RemoveValue {
 	
 	public static List<Integer> removeValue(int [] numbs, int value) {
 		
+		
+		//with List
 		//convert numbs into a List
 		List<Integer> numbsList = new ArrayList <>();
 		for (Integer integer : numbs) {
 			numbsList.add(integer);
 		}
 		
-		Iterator<Integer> numbsListIterator = numbsList.iterator();
 		
+		//using the iterator to remove the value's elements 
+		Iterator<Integer> numbsListIterator = numbsList.iterator();
 		while (numbsListIterator.hasNext()) {
 			Integer i = numbsListIterator.next();
 			if(i == value) {
 				numbsListIterator.remove();
 			}
 		}
+		
+		
+		//Without List (you actually need list 'cause don't know how long will be the result
+		int[] numbsWithoutValue = new int[numbs.length];
+		for (int i = 0, j=0; i < numbs.length; i++) {
+			if(numbs[i] != value) {
+				numbsWithoutValue[j] = numbs[i];
+			}
+			j++;
+		}
+		
 		
 		return numbsList;
 		
